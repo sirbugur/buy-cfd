790b7bfd1e919a5a62307a4b5c448055848077ed
sirbugur <bugur.yazici@gmail.com>
2020-10-26 16:17:56 +0300

    Adding template files

---
+++ deneme.cpp
@@ -0,0 +1,6 @@
+#include "deneme.hpp"
+
+double bugur::sum_buy(double a, double b)
+{
+    return a + b;
+}
---
+++ deneme.hpp
@@ -0,0 +1,7 @@
+namespace bugur
+{
+    double sum_buy(double a, double b)
+    {
+        return a + b;
+    }
+} // namespace bugur
